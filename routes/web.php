f810aef2fac1fe000c2e72f6589b1e7047d751a2
RonyKhanHM <Rajakhanhmr@gmail.com>
2024-09-06 03:16:18 +0600

    This is php Auth login Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\AuthController;
 use App\Http\Controllers\Frontend\FrontendController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 
+//Frontend url
 Route::get('/', [FrontendController::class, 'index']);
 Route::get('/product/details', [FrontendController::class, 'productDetails']);
 Route::get('/view-cart', [FrontendController::class, 'viewCart']);
 Route::get('/checkout', [FrontendController::class, 'checkout']);
+
+Auth::routes();
+
+//Admin login url
+Route::get('/admin/login', [AuthController::class, 'adminLogin'])->name('adminLogin');
+
+// Admin Pannel
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('adminDashboard');
